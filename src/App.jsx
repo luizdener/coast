39f3d4f97a66709b84c70ca03fdad5bbfdc9194a
luizdener <ldenerjesus@gmail.com>
2023-08-25 18:33:56 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './styles/globals.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Projeto Coast</h1>
+      <p>coasts</p>
+    </div>
+  )
+}
+
+export default App
